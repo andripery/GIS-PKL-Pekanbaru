dddf8e8120b9a31fbab717797ae9769d6904b466
andripery <andripery@gmail.com>
2020-01-05 17:19:10 +0700

    Hide Marker

--- html/maptest.php
+++ html/maptest.php
@@ -23,15 +23,10 @@
         var peta = new L.TileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png');
         var bw = new L.tileLayer('http://{s}.tiles.wmflabs.org/bw-mapnik/{z}/{x}/{y}.png');
 
-        var baseMaps = {
-            "Mapnik": peta,
-            "Black & White" : bw
-        };
-
-        // Layer control
-        L.control.layers(baseMaps, baseMaps).addTo(map);
         // Adding layer to the map
         map.addLayer(peta);
+
+        var marker = L.featureGroup();
 
         <?php
         require('koneksi.php');
@@ -40,17 +35,38 @@
         $data = mysqli_query($db, $sql);
 
         $js = '';
+        $array = array();
 
         // looping script js ini sesuai dengan jumlah lokasi yang ada pada database
         while ($row =  mysqli_fetch_array($data)) {
             $js .= 'L.marker([' . $row['features_properties_Y'] . ', ' . $row['features_properties_X'] .
-                ']).addTo(map).bindPopup("<b>' . $row['features_properties_Nama_Pemet'] . '</b> <br><b>' .
+                ']).addTo(marker).bindPopup("<b>' . $row['features_properties_Nama_Pemet'] . '</b> <br><b>' .
                 $row['features_properties_Alamat'] . '</b> <br><img width=100 src=' . $row['features_properties_Foto'] .
                 '>");';
+            // $array[] .= 'L.marker([' . $row['features_properties_Y'] . ', ' . $row['features_properties_X'] .
+            //     ']).addTo(map).bindPopup("<b>' . $row['features_properties_Nama_Pemet'] . '</b> <br><b>' .
+            //     $row['features_properties_Alamat'] . '</b> <br><img width=100 src=' . $row['features_properties_Foto'] .
+            //     '>");';
         }
         // menampilkan script js hasil dari looping diatas
         echo $js;
+        // print_r($array);
+        // echo $array[0];
         ?>
+        
+        map.addLayer(marker);
+
+        var baseMaps = {
+            "Mapnik": peta,
+            "Black & White": bw
+        };
+
+        var overlayMaps = {
+            "Marker": marker
+        };
+
+        // Layer control
+        L.control.layers(baseMaps, overlayMaps).addTo(map);
     </script>
 </body>
 
